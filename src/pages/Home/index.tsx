1d8876ab083787ad263436fcfd2c5e3b1c38dc6d
dzmtrmnvsk <dimadi2468@gmail.com>
2025-01-20 21:02:25 +0300

    feat: for you arts component created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,30 @@
-import MuseumSearch from '@/components/MuseumSearch'
+import { Suspense, lazy } from 'react'
 import PageLayout from '@/components/PageLayout'
 import { ErrorBoundary } from '@/components/ErrorBoundary'
 import ErrorFallback from '@/components/ErrorFallback'
-import SpecialArts from '@/components/SpecialArts'
+import Loader from '@/components/Loader'
+
+const MuseumSearch = lazy(() => import('@/components/MuseumSearch'))
+const SpecialArts = lazy(() => import('@/components/SpecialArts'))
+const ForYouArts = lazy(() => import('@/components/ForYouArts'))
 
 const HomePage = () => {
   return (
     <PageLayout>
       <ErrorBoundary fallback={(error) => <ErrorFallback error={error} />}>
-        <MuseumSearch />
+        <Suspense fallback={<Loader />}>
+          <MuseumSearch />
+        </Suspense>
       </ErrorBoundary>
       <ErrorBoundary fallback={(error) => <ErrorFallback error={error} />}>
-        <SpecialArts />
+        <Suspense fallback={<Loader />}>
+          <SpecialArts />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary fallback={(error) => <ErrorFallback error={error} />}>
+        <Suspense fallback={<Loader />}>
+          <ForYouArts />
+        </Suspense>
       </ErrorBoundary>
     </PageLayout>
   )
