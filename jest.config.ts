50e91ede8607bc71929291453cf19a56ecbe0e40
dzmtrmnvsk <dimadi2468@gmail.com>
2025-01-22 18:14:37 +0300

    test: write tests for utils

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.app.json'
+      }
+    ]
+  },
+
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.svg$': 'jest-transformer-svg',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@contexts/(.*)$': '<rootDir>/src/contexts/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1'
+  },
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+}
